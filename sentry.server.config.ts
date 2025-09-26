a4c9326cbcc08822304ebdd63ed8a2ef7539bc9c
Ranajit Chandra <ranajit.chandra@walmart.com>
2025-09-26 10:36:15 +0530

    feat(): added personal teacher application

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2cb5557e9e05f796df72478615505721@o4510083478061056.ingest.us.sentry.io/4510083722969088",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
